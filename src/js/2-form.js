aaefbfd303a21c73c3ca7f1fa6425b4488e7b029
ElenaUM <luzunova99@gmail.com>
2024-06-04 18:18:04 +0300

    додано html, css та js

---
+++ src/js/2-form.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('.feedback-form');
+const formData = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {
+  email: '',
+  message: '',
+};
+form.elements.email.value = formData.email.trim();
+form.elements.message.value = formData.message.trim();
+
+form.addEventListener('input', () => {
+  formData.email = form.elements.email.value.trim();
+  formData.message = form.elements.message.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (
+    !form.elements.email.value.trim() ||
+    !form.elements.message.value.trim()
+  ) {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+});
